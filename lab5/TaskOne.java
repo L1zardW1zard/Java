8a1db24db47a78ce7b863d1ee3b93c9fc81cea84
L1zardW1zard <45181494+L1zardW1zard@users.noreply.github.com>
2021-12-21 19:44:03 +0200

    labs

--- lab5/TaskOne.java
+++ lab5/TaskOne.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class TaskOne {
 
     public static void main(String[] args) {
-        System.out.println(findUniq(new double[] {1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1}));
-        System.out.println(findUniq(new double[] {0, 0, 0.55, 0, 0}));
-        System.out.println(findUniq(new double[] {8, 1, 1, 1, 1, 1}));
-        System.out.println(findUniq(new double[] {1, 1, 4}));
-        System.out.println(findUniq(new double[] {1, 1, 1, 2, 1, 1}));
+        System.out.println(findUnique(new double[] {1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1}));
+        System.out.println(findUnique(new double[] {0, 0, 0.55, 0, 0}));
+        System.out.println(findUnique(new double[] {8, 1, 1, 1, 1, 1}));
+        System.out.println(findUnique(new double[] {1, 1, 4}));
+        System.out.println(findUnique(new double[] {1, 1, 1, 2, 1, 1}));
     }
 
-    private static double findUniq(double[] array) {
+    private static double findUnique(double[] array) {
         double[] targetArray = Arrays.stream(array).sorted().toArray();
         return targetArray[targetArray.length - 1];
     }
