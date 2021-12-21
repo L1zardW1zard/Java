38d71f6f56ded1959b18c4a1c06feb73bc1a09c2
L1zardW1zard <45181494+L1zardW1zard@users.noreply.github.com>
2021-12-21 18:48:40 +0200

    labs

--- lab2/lab_2/Main.java
+++ lab2/lab_2/Main.java
@@ -5,33 +5,33 @@ import java.io.Serializable;
 public class Main {
     public static void main(String[] args)  {
        LinkedList<Integer> list = new LinkedList<Integer>();
-        list.add(3);
-        list.add(5);
-        list.add(7);
-        list.add(9);
+        list.add(2);
+        list.add(4);
         list.add(6);
+        list.add(8);
+        list.add(5);
 
         printArray(list.toArray());
         System.out.println("");
-        System.out.println(list.contains(9));
-        System.out.println(list.contains(-5));
-        list.remove(7);
+        System.out.println(list.contains(1));
+        System.out.println(list.contains(-3));
+        list.remove(8);
         printArray(list.toArray());
 
         ArrayList<Double> arrayList = new ArrayList<Double>();
-        arrayList.add(3.0);
-        arrayList.add(8.0);
+        arrayList.add(4.0);
         arrayList.add(7.0);
+        arrayList.add(2.0);
         arrayList.add(10.0);
-        arrayList.add(3.0);
+        arrayList.add(43.0);
+        arrayList.add(21.0);
+        arrayList.add(6.0);
+        arrayList.add(1.0);
+        arrayList.add(5.0);
+        arrayList.add(76.0);
         arrayList.add(8.0);
-        arrayList.add(7.0);
+        arrayList.add(6.0);
         arrayList.add(3.0);
-        arrayList.add(8.0);
-        arrayList.add(7.0);
-        arrayList.add(3.0);
-        arrayList.add(8.0);
-        arrayList.add(7.0);
         System.out.println("");
         printArray(arrayList.toArray());
         System.out.println("");
@@ -39,8 +39,8 @@ public class Main {
 
         TestClass tc = new TestClass();
         byte[] bytes = SerializationUtil.serialize(tc);
-        TestClass deserelizedTestClas = (TestClass) SerializationUtil.deserialize(bytes);
-        System.out.print(deserelizedTestClas.name + " " + deserelizedTestClas.age);
+        TestClass deserelizedTestClass = (TestClass) SerializationUtil.deserialize(bytes);
+        System.out.print(deserelizedTestClass.name + " " + deserelizedTestClass.age);
     }
 
     private static <T> void printArray(T[] arr) {
